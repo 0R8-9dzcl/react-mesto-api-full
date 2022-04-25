d6ec6a6177108bd84a6ff784fbee46853af8e38f
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-04-25 15:10:31 +0300

    fix: Добавит заголовки

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -14,7 +14,8 @@ export const register = (password, email) => {
 	return fetch(`${BASE_URL}/signup`, {
 		method: 'POST',
 		headers: {
-			'Content-Type': 'application/json'
+			'Content-Type': 'application/json',
+			'credentials': 'include',
 		},
 		body: JSON.stringify({ password, email })
 	})
@@ -25,19 +26,20 @@ export const login = (password, email) => {
 	return fetch(`${BASE_URL}/signin`, {
 		method: 'POST',
 		headers: {
-			'Content-Type': 'application/json'
+			'Content-Type': 'application/json',
+			'credentials': 'include',
 		},
 		body: JSON.stringify({ password, email })
 	})
 	.then(res => checkOk(res))
 }
 
-export const getAuthorization = (token) => {
+export const getAuthorization = () => {
 	return fetch(`${BASE_URL}/users/me`, {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
-			"Authorization" : `Bearer ${token}`
+			'credentials': 'include',
 		}
 	})
 	.then(res => checkOk(res))
