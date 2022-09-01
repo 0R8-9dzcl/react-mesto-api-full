b70e878ba3e7b3851eec892e16045fc019b98505
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-09-02 00:45:41 +0300

    feat: обновить год

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
 	return (
 		<footer className="footer">
-				<p className="footer__copy">&copy; 2021 Mesto Russia</p>
+				<p className="footer__copy">&copy; {new Date().getFullYear()} Mesto Russia</p>
 		</footer>
 	)
 }
